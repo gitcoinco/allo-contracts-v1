74261e455d6ab00cc80297d07de3f577a06f091d
Andrea Franz <andrea@gravityblast.com>
2023-10-24 03:11:40 +0200

    update dev deterministic contract addresses

--- scripts/config/round.config.ts
+++ scripts/config/round.config.ts
@@ -11,8 +11,8 @@ type DeployParams = Record<string, RoundParams>;
 
 export const roundParams: DeployParams = {
   dev: {
-    roundFactoryContract: "0x59b670e9fA9D0A427751Af201D676719a970857b",
-    roundImplementationContract: "0x4ed7c70F96B99c776995fB64377f0d4aB3B0e1C1",
+    roundFactoryContract: "0x322813Fd9A801c5507c9de605d63CEA4f2CE6c44",
+    roundImplementationContract: "0xa85233C63b9Ee964Add6F2cffe00Fd84eb32338f",
     roundContract: "",
   },
   mainnet: {
